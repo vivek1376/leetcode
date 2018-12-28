e0243b5e3e1b90521a3d3ec1446091d5351b8830
Vivek Mehra <vivek.1376@gmail.com>
2018-12-28 01:37:18 -0500

    generate-parentheses.py : added prints to display recursion tree

--- generate-parentheses.py
+++ generate-parentheses.py
@@ -7,33 +7,22 @@ class Solution:
         self.parenStrList = []
         self.n = 0
 
-    def func(self, parenStr, numOpen, numClose):
+    def func(self, funcNo, indent, parenStr, numOpen, numClose):
         #print("func: " + str(n_old))
-
+        print(' ' * indent + "func" + str(funcNo) + ": " + str(parenStr))
         if len(parenStr) == self.n * 2:
             self.parenStrList.append(parenStr)
             return
 
-        
-        if numOpen == numClose:
+        if numOpen < self.n:
             parenStr2 = parenStr[:]
             parenStr2 += '('
-            self.func(parenStr2, numOpen + 1, numClose)
+            self.func(1, indent+2, parenStr2, numOpen + 1, numClose)
 
-        elif numOpen > numClose and numOpen == self.n:
-            # backtracking
+        if numOpen > numClose:
             parenStr2 = parenStr[:]
             parenStr2 += ')'
-            self.func(parenStr2, numOpen, numClose + 1)
-
-        elif numOpen > numClose and numOpen < self.n:
-            parenStr2 = parenStr[:]
-            parenStr2 += '('
-            self.func(parenStr2, numOpen + 1, numClose)
-
-            parenStr2 = parenStr[:]
-            parenStr2 += ')'
-            self.func(parenStr2, numOpen, numClose + 1)
+            self.func(2, indent+2, parenStr2, numOpen, numClose + 1)
 
 
         return self.parenStrList
@@ -46,7 +35,7 @@ class Solution:
         """
         #newParenList = []
         self.n = n
-        return self.func("", 0, 0)
+        return self.func(0, 0, "", 0, 0)
             
             
         
