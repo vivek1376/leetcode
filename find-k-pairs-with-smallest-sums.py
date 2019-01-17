efb8e58a875c8bd580717971b64ddf75495c00d5
Vivek Mehra <vivek.1376@gmail.com>
2019-01-17 03:27:06 -0500

    find-k-pairs-with-smallest-sums.py : another approach

--- find-k-pairs-with-smallest-sums.py
+++ find-k-pairs-with-smallest-sums.py
@@ -14,38 +14,36 @@ class Solution:
         :rtype: List[List[int]]
         """
 
-        def myFunc(e):
-            return e[0] + e[1]
-
-
+        l2 = len(nums2)
+        
+        index2 = [0 for i in range(len(nums1))]
         kPairs = []
-        pairSumHeap = []
 
-        # maxPairSum = -9999999999999999999999
+        for i1 in range(k):
+            minVal = 999999999999999
+            minIndex = -1
             
-        for val1 in nums1[:k]:
-            # print("val1: " + str(val1))
-            for val2 in nums2[:k]:
-                # print("nums2[i2]: " + str(nums2[i2]))
+            if k > len(nums1):
+                l1 = len(nums1)
+            else:
+                l1 = k
+                
+            for i in range(0, l1):
+                # print("i: " + str(i) + " index2[i]: " + str(index2[i]))
+                if index2[i] < l2 and nums1[i] + nums2[index2[i]] < minVal:
+                    minVal = nums1[i] + nums2[index2[i]]
+                    minIndex = i
 
-                if len(kPairs) < k:
-                    # print("IF")
-                    kPairs.append([val1, val2])
-                    heapq.heappush(pairSumHeap, -1 * (val1 + val2))
+            if not minIndex == -1:
+                kPairs.append([nums1[minIndex], nums2[index2[minIndex]]])
+                index2[minIndex] += 1
 
-
-                elif val1 + val2 < (-1 * pairSumHeap[0]):
-                    # print("ELIF")
-                    heapq.heappop(pairSumHeap)
-                    heapq.heappush(pairSumHeap, -1 * (val1 + val2))
-                    kPairs.append([val1, val2])
-
-        kPairs.sort(key=myFunc)
-
-        return(kPairs[:k])
-
+            # print("kPairs: " + str(kPairs))
+            
+        return(kPairs)
 
 
 
 s = Solution()
-print(s.kSmallestPairs([1,2,3], [10,20,30], 3))
+#print(s.kSmallestPairs([1,2,3], [10,20,30], 3))
+print(s.kSmallestPairs([1,2,4,5,6],[3,5,7,9], 20))
